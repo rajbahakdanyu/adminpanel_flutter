2343ab6c77bcc2d34aa362238520a6b453882d12
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-02-10 19:58:57 +0545

    Moved colors to constant file

--- lib/constants/colors.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Colors.purple;
-const primaryAccent = Colors.purpleAccent;
--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,1 +1,6 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor primaryColor = Colors.purple;
+const MaterialAccentColor primaryAccent = Colors.purpleAccent;
+
 const double padding = 16.0;
