405965e81147ecaa6796bc670361bb5342c64905
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-02-10 16:08:32 +0545

    Changed title bar colors

--- lib/widgets/window_title_bar.dart
+++ lib/widgets/window_title_bar.dart
@@ -1,19 +1,18 @@
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 
+import '/constants/colors.dart';
+
 final buttonColors = WindowButtonColors(
-  iconNormal: const Color(0xFF805306),
-  mouseOver: const Color(0xFFF6A00C),
-  mouseDown: const Color(0xFF805306),
-  iconMouseOver: const Color(0xFF805306),
-  iconMouseDown: const Color(0xFFFFD500),
+  iconNormal: primaryColor,
+  mouseOver: primaryColor.shade200,
+  mouseDown: primaryColor.shade300,
 );
 
 final closeButtonColors = WindowButtonColors(
   mouseOver: const Color(0xFFD32F2F),
   mouseDown: const Color(0xFFB71C1C),
-  iconNormal: const Color(0xFF805306),
-  iconMouseOver: Colors.white,
+  iconNormal: primaryColor,
 );
 
 class WindowButtons extends StatelessWidget {
