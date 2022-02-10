3468fa2a069e5fff07692c2296c6c1beff3dac7c
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-02-10 16:59:31 +0545

    Added list in drawer

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -12,12 +12,28 @@ class SideDrawer extends StatelessWidget {
           SizedBox(height: MediaQuery.of(context).size.height * .05),
           FaIcon(
             FontAwesomeIcons.userCircle,
-            size: MediaQuery.of(context).size.height * .06,
+            size: MediaQuery.of(context).size.width * .04,
           ),
           Text(
             'Admin Panel',
-            style: Theme.of(context).textTheme.headline5,
+            style: Theme.of(context).textTheme.headline5!.copyWith(
+                  fontSize: MediaQuery.of(context).size.width * .015,
+                ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * .7,
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return const ListTile(
+                    title: Text('Hello'),
+                  );
+                },
+              ),
+            ),
+          )
         ],
       ),
     );
