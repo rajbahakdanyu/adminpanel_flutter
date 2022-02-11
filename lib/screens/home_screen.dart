593e23e7d5ef8de7a60c2bf3cb98534af3439966
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-02-11 15:45:50 +0545

    Made gridview responsive

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:adminpanel/constants/constant.dart';
+import 'package:adminpanel/utils/responsive.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,7 +14,20 @@ class HomeScreen extends StatelessWidget {
         SizedBox(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          child: const InfoRow(),
+          child: const Responsive(
+            mobile: InfoRow(
+              crossCount: 2,
+              aspectRatio: 21 / 9,
+            ),
+            tablet: InfoRow(
+              crossCount: 4,
+              aspectRatio: 8 / 1,
+            ),
+            desktop: InfoRow(
+              crossCount: 4,
+              aspectRatio: 16 / 9,
+            ),
+          ),
         ),
       ],
     );
@@ -23,24 +37,52 @@ class HomeScreen extends StatelessWidget {
 class InfoRow extends StatelessWidget {
   const InfoRow({
     Key? key,
+    required this.crossCount,
+    required this.aspectRatio,
   }) : super(key: key);
+
+  final int crossCount;
+  final double aspectRatio;
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       physics: const NeverScrollableScrollPhysics(),
       itemCount: 4,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossCount,
         crossAxisSpacing: padding / 2,
         mainAxisSpacing: padding / 2,
-        childAspectRatio: 1,
+        childAspectRatio: aspectRatio,
       ),
       itemBuilder: (context, index) {
-        return Card(
-          child: Text(index.toString()),
-        );
+        return const InfoCard();
       },
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  const InfoCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: primaryColor,
+      child: Padding(
+        padding: const EdgeInsets.all(padding / 2),
+        child: Row(
+          children: const [
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(padding / 2),
+                child: Icon(Icons.person),
+              ),
+            ),
+            Text('Item'),
+          ],
+        ),
+      ),
     );
   }
 }
